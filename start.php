0968536767fda22d2fc6695c16ce60a08beff151
AgonyMidnight <agony.midnight.am@gmail.com>
2020-08-27 22:27:05 +0300

    to start

---
+++ start.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * 1) вывести все продукты у которых attribute_code = page_layout. Выборку сделайте по следующим полям:
+ *   entity_id, sku, created_at, value, attribute_code
+( entity_id, sku, created_at из catalog_product_entity, attribute_code из eav_attribute
+value из catalog_product_entity_text
+)
+используйте обьединение через таблицу catalog_product_entity_text
+используйте алиасы для таблиц
+
+2) вывести все продукты (catalog_product_entity) вместе с текстом (catalog_product_entity_text) от 2017-09-20 16:03:28
+
+3) вывести все поля: entity_id, sku, created_at, value
+(entity_id, sku, created_at из catalog_product_entity
+value из catalog_product_entity_text
+)
+в которых sku есть '11'
+ */
+
+require_once DIR . '/vendor/autoload.php';
